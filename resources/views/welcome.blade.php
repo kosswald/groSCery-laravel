bcdee13c5625941806ac6f8ee2861315bdbb8963
Kristof Osswald <kristofosswald@gmail.com>
2018-10-28 17:22:17 -0700

    New front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    groSCery API
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <h1>Documentation In Progress</h1>
+                <h3>/exampleurl</h3>
+                Request parameter: </br>
+                Return: 
             </div>
         </div>
     </body>
